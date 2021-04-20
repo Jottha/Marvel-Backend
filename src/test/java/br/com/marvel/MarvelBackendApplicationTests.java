c0c078425f0459bec2d4332eba8866a32de279fc
jailton.ventura.silva <jottha_2006@hotmail.com>
2021-04-19 23:35:27 -0300

    Package architecture

---
+++ src/test/java/br/com/marvel/MarvelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
